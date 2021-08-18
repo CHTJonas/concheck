1f468de463dd2d3923a519c8fdf2ebac2211f03b
Charlie Jonas <charlie@charliejonas.co.uk>
2021-08-18 23:32:52 +0100

    Make it concurrent

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"net/http"
 	"os"
+	"sync"
 )
+
+var wg sync.WaitGroup
+
+func check(url string) {
+	_, err := http.Get(url)
+	if err != nil {
+		os.Exit(1)
+	}
+	wg.Done()
+}
 
 func main() {
 	URLs := []string{"https://captive.apple.com/", "https://connectivitycheck.gstatic.com/generate_204"}
 	for _, url := range URLs {
-		_, err := http.Get(url)
-		if err != nil {
-			os.Exit(1)
-		}
+		wg.Add(1)
+		go check(url)
 	}
+	wg.Wait()
 }
