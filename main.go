2f9cb6295ac039e6011a75513623adb618c460cd
Charlie Jonas <charlie@charliejonas.co.uk>
2021-08-18 23:57:47 +0100

    Add more test URLs

--- main.go
+++ main.go
@@ -17,7 +17,12 @@ func check(url string) {
 }
 
 func main() {
-	URLs := []string{"https://captive.apple.com/", "https://connectivitycheck.gstatic.com/generate_204"}
+	URLs := []string{
+		"https://chtj2.user.srcf.net/static",
+		"https://captive.apple.com/",
+		"https://connectivitycheck.gstatic.com/generate_204",
+		"https://cloudflare.com/cdn-cgi/trace",
+	}
 	for _, url := range URLs {
 		wg.Add(1)
 		go check(url)
